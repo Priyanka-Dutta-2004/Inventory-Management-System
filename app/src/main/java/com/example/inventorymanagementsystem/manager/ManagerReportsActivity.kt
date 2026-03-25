8f1868e4ba0ae7d501688463f74367b2450a6451
Priyanka Dutta <priyankadutta2004@gmail.com>
2026-03-25 15:35:38 +0530

    Making the subpages and connecting them

---
+++ app/src/main/java/com/example/inventorymanagementsystem/manager/ManagerReportsActivity.kt
@@ -0,0 +1,29 @@
+package com.example.inventorymanagementsystem.manager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inventorymanagementsystem.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class ManagerReportsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_manager_reports)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
